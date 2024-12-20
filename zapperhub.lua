23ddc65631faf7213f575d027d643d4c907ad454
snacktheovercomplicated <omarahmed81222@gmail.com>
2024-12-20 08:25:47 +0200

    i fixed the local part:sad:

--- zapperhub.lua
+++ zapperhub.lua
@@ -1,9 +1,9 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "zapperhub", HidePremium = false, SaveConfig = true, ConfigFolder = "test"})
-local Tab = Window:MakeTab({
+ loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+ OrionLib:MakeWindow({Name = "zapperhub", HidePremium = false, SaveConfig = true, ConfigFolder = "test"})
+ Window:MakeTab({
 	Name = "Zapper?",
 	Icon = "rbxassetid://18383452484",
 	PremiumOnly = false
-local Section = Tab:AddSection({
+ Tab:AddSection({
 	Name = "test 2"
 })
