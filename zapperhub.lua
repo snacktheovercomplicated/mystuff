1082073cf19d6ce0ae55e7fb2334f9a5042f8568
snacktheovercomplicated <omarahmed81222@gmail.com>
2024-12-20 10:45:01 +0200

    done stuff with solara after a bunch of brianstorming

--- zapperhub.lua
+++ zapperhub.lua
@@ -1,16 +1,39 @@
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-OrionLib:MakeWindow({Name = "zapperhub", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
-Window:MakeTab({
-	Name = "Tab 1",
-	Icon = "rbxassetid://18383452484",
-	PremiumOnly = false
+local player = game.Players.LocalPlayer
+
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "zapperhub", HidePremium = false, SaveConfig = true, ConfigFolder = "zapperhub"})
+
+local Tab = Window:MakeTab({
+    Name = "robloxy stuff",
+    Icon = "rbxassetid://18383452484",
+    PremiumOnly = false
+
+}
+)
+Tab:AddSlider({
+    Name = "walkspeed",
+    Min = 16,
+    Max = 5000,
+    Default = 15,
+    Color = Color3.fromRGB(9, 24, 28),
+    Increment = 1,
+    ValueName = "speed",
+    Callback = function(Value)
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+    end
+
 })
-Tab:AddSection({
-	Name = "other guis"
-Tab:AddButton({
-	Name = "storgen hub",
-	Callback = function(loadstring(game:HttpGet("https://raw.githubusercontent.com/giogio11223/Storgenhubkeysystem/refs/heads/main/gioyuhh",true))())
-      		print("loaded storgen hub")
-  	end    
+
+Tab:AddSlider({
+    Name = "jump",
+    Min = 50,
+    Max = 2500,
+    Default = 50,
+    Color = Color3.fromRGB(9, 24, 28),
+    Increment = 1,
+    ValueName = "jump power",
+    Callback = function(Value)
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+    end
 })
-		
